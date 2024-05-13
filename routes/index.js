99e563e14c00f0ce80a1d4eca5297c9ab3852e1a
sirclek <kelvinsofjan@gmail.com>
2024-05-13 19:32:34 +1000

    user parameter added

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render("index", { title: "IoTBay" , user: req.session.user});
+
 });
 
 module.exports = router;
