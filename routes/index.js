b1b4096db6839cabebcb4a523b9ac4c6396bce2c
micheleliang <micheleliang16@gmail.com>
2024-05-14 17:56:29 +1000

    updated navbar so it can be repeated in other pages without repeating code

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render("index", { title: "IoTBay" , user: req.session.user});
+  res.render("index", { title: "Home | IoTBay" , user: req.session.user, activePage: 'home'});
 
 });
 
